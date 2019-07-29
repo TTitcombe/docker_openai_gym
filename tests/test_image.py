c2eb3c3a5855d829350cde5fac4ca8ab6e728557
Thomas Titcombe <thomas.titcombe@tessella.com>
2019-07-29 13:23:09 +0100

    Add requirements for box2d to dockerfile and requirements.txt
    Add unit test to check box2d env

--- tests/test_image.py
+++ tests/test_image.py
@@ -7,8 +7,8 @@ class TestImage(unittest.TestCase):
     def test_can_make_CartPole_env(self):
         gym.make("CartPole-v1")
 
-    def xtest_can_make_LunarLander(self):
-        gym.make("LunarLander-v0")
+    def test_can_make_LunarLander_env(self):
+        gym.make("LunarLander-v2")
 
 
 if __name__ == "__main__":
